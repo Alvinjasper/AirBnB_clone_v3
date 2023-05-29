d5fb546bfd092d843161d8f3dc1e8caedbabbe34
Alvinjasper <alvinjspr@gmail.com>
2023-05-29 14:55:53 +0000

    all tasks

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
-"""Initialize Blueprint views"""
-from flask import Blueprint
+"""index.py to connect to API"""
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from models import storage
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.places import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places_reviews import *
-from api.v1.views.states import *
-from api.v1.views.users import *
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
